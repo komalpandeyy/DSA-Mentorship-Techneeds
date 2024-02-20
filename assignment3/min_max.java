86c23a60927230362fcbb94ffd369a5e067caf10
Komal Pandey <83563937+komalpandeyy@users.noreply.github.com>
2024-02-20 15:55:01 +0530

    Update min_max.java

--- assignment3/min_max.java
+++ assignment3/min_max.java
@@ -1,1 +1,20 @@
+class Solution 
+{
+    static Pair getMinMax(long a[], long n)  
+    {
+        //Write your code here
+         long min = a[0];
+         long max = a[0];
+         for(long ele:a){
+             if(min>ele){
+                 min = ele;
+             }
+             if(max<ele){
+                 max = ele;
+             }
+         }
+          return new Pair(min,max);
+    }
+   
+}
 
